8eb9242aec4d19bdc76ea2e3d82568bf0124c997
SavaN22 <savanakarada22@gmail.com>
2021-04-04 01:00:42 +0200

    Student Repository findByEmail

---
+++ src/main/java/com/nakaradasava/learntogether/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.nakaradasava.learntogether.repository;
+
+import com.nakaradasava.learntogether.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    Optional<Student> findByEmail(String email);
+}
