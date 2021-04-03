c42e626c92728915c9ca59a1c7baed74ac921cba
SavaN22 <savanakarada22@gmail.com>
2021-04-04 01:38:09 +0200

    Change password encoder to BcRypt

--- src/main/java/com/nakaradasava/learntogether/config/PasswordConfig.java
+++ src/main/java/com/nakaradasava/learntogether/config/PasswordConfig.java
@@ -2,14 +2,13 @@ package com.nakaradasava.learntogether.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class PasswordConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
