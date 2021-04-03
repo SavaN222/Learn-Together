d583401b3257ac0814628c91ff3008d0090cc869
SavaN22 <savanakarada22@gmail.com>
2021-04-04 01:15:20 +0200

    WebConfig implementation for security and auth

---
+++ src/main/java/com/nakaradasava/learntogether/config/WebConfig.java
@@ -0,0 +1,48 @@
+package com.nakaradasava.learntogether.config;
+
+import com.nakaradasava.learntogether.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebConfig extends WebSecurityConfigurerAdapter {
+
+    private StudentService studentService;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public WebConfig(StudentService studentService, PasswordEncoder passwordEncoder) {
+        this.studentService = studentService;
+        this.passwordEncoder = WebConfig.this.passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/register/**")
+                    .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider())
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(studentService);
+        return provider;
+    }
+}
