04c04b689afc1dbfe76113e6bd6d99f8bdf3690f
SavaN22 <savanakarada22@gmail.com>
2021-04-04 00:49:51 +0200

    Starter files for Spring Boot

---
+++ src/main/java/com/nakaradasava/learntogether/LearnTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.nakaradasava.learntogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nakaradasava/learntogether/LearnTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakaradasava.learntogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
