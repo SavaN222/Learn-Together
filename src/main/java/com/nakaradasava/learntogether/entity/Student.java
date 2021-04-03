ddc1d13be0237d8f77f038afa14eba0d05097fab
SavaN22 <savanakarada22@gmail.com>
2021-04-04 00:54:15 +0200

    Student entity

---
+++ src/main/java/com/nakaradasava/learntogether/entity/Student.java
@@ -0,0 +1,37 @@
+package com.nakaradasava.learntogether.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @Column(name = "profile_pic")
+    private String profilePic;
+}
