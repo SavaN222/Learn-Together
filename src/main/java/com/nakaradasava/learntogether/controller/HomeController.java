cdcc1fa22b76e6be0399c5216926716549a7b356
SavaN22 <savanakarada22@gmail.com>
2021-04-11 13:37:07 +0200

    Error handling for QUOTE API

--- src/main/java/com/nakaradasava/learntogether/controller/HomeController.java
+++ src/main/java/com/nakaradasava/learntogether/controller/HomeController.java
@@ -6,13 +6,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.UnknownHostException;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
     public String index(Model model) {
         RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject("https://api.quotable.io/random", Quote.class);
+        Quote quote;
+
+        try {
+            quote = restTemplate.getForObject("https://api.quotable.io/random", Quote.class);
+        } catch (Exception exception) {
+            quote = new Quote("Dalai Lama", "Share your knowledge. It is a way to achieve immortality.");
+        }
 
         model.addAttribute("quote", quote);
         return "index";
