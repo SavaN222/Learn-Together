457d7dda69ae0839898aee47364436223e86f232
SavaN22 <savanakarada22@gmail.com>
2021-04-14 16:36:45 +0200

    List all post for student for university

--- src/main/java/com/nakaradasava/learntogether/controller/university/UniversityController.java
+++ src/main/java/com/nakaradasava/learntogether/controller/university/UniversityController.java
@@ -4,6 +4,7 @@ import com.nakaradasava.learntogether.entity.university.LikePost;
 import com.nakaradasava.learntogether.entity.university.Post;
 import com.nakaradasava.learntogether.entity.university.University;
 import com.nakaradasava.learntogether.entity.student.Student;
+import com.nakaradasava.learntogether.service.university.PostService;
 import com.nakaradasava.learntogether.service.university.UniversityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,14 +12,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
 
 @Controller
 public class UniversityController {
     private UniversityService universityService;
+    private PostService postService;
 
     @Autowired
-    public UniversityController(UniversityService universityService) {
+    public UniversityController(UniversityService universityService, PostService postService) {
         this.universityService = universityService;
+        this.postService = postService;
     }
 
     @GetMapping("/university/{id}")
@@ -31,9 +35,12 @@ public class UniversityController {
         }
 
         University university = universityService.findUniversity(id);
+        List<Post> posts = postService.findPostsByStudent(student);
+
         model.addAttribute("university", university);
         model.addAttribute("post", new Post());
         model.addAttribute("like", new LikePost());
+        model.addAttribute("studentPosts", posts);
 
         return "university";
 
--- src/main/java/com/nakaradasava/learntogether/repository/university/PostRepository.java
+++ src/main/java/com/nakaradasava/learntogether/repository/university/PostRepository.java
@@ -1,9 +1,12 @@
 package com.nakaradasava.learntogether.repository.university;
 
+import com.nakaradasava.learntogether.entity.student.Student;
 import com.nakaradasava.learntogether.entity.university.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findAllByStudent(Student student);
 }
