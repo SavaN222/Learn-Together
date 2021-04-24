12075db2607feaa9d51f361b0576faa2d4397a95
SavaN22 <savanakarada22@gmail.com>
2021-04-24 14:30:04 +0200

    remove unneccessary package

--- src/main/java/com/nakaradasava/learntogether/controller/FavoriteController.java
+++ src/main/java/com/nakaradasava/learntogether/controller/FavoriteController.java
@@ -14,7 +14,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Controller
